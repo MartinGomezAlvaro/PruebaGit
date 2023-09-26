b62c002559189a344f93f3660cb346479719dcc6
DAM <alvaromartingomez2511@gmail.com>
2023-09-26 08:19:05 +0200

    He creado la clase empleado

---
+++ src/Empleado.java
@@ -0,0 +1,16 @@
+public class Empleado {
+    int id;
+    String nombre;
+
+    public Empleado(int id){
+        this.id=id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
